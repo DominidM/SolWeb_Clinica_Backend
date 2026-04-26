46610311a79a4ad0cee727cd61cd4c567a486287
Dominid Muñoz Eslava <dominidzero@gmail.com>
2026-04-26 08:04:03 -0500

    beta

---
+++ backend/src/test/java/edu/upn/clinica/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.upn.clinica.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
